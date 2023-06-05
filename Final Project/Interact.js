bae2216bac95a8a359cc651a239b05822d08fa06
olivianairn18 <129685915+olivianairn18@users.noreply.github.com>
2023-06-05 09:08:16 +0100

    Add files via upload

---
+++ Final Project/Interact.js
@@ -0,0 +1,11 @@
+document.getElementById("myButton").onclick = function () {
+    alert("Nope");
+}
+
+document.getElementById("changeText").onclick = function () {
+    document.getElementById("changeText").innerHTML = "See? Very useless";}
+
+
+
+
+
